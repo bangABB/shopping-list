3ce436ad353df6c782c6a7ac129c40f27e3e974e
bangABB <arrayabless@gmail.com>
2023-09-27 03:19:59 +0700

    Bisa dong hehhe

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\PBPGut\shopping_list\env
+set VIRTUAL_ENV=D:\shopping_list\env
 
 if not defined PROMPT set PROMPT=$P$G
 
